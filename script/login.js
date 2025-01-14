0433efdf24e623a925e8cc02f93c90f9587faf1b
CancerAi <cancerai@163.com>
2025-01-14 11:53:57 +0800

    finish: registor page

--- script/login.js
+++ script/login.js
@@ -10,3 +10,21 @@ function login() {
         }
     }
 }
+
+function reg() {
+    let username = document.getElementById("username");
+    let passwd = document.getElementById("passwd");
+    let deter_passwd = document.getElementById("deter_passwd");
+
+    if (username.value == "" || passwd.value == "" || deter_passwd == "") {
+        alert("账号密码为空，重新输入");
+    }else {
+        if (passwd.value == deter_passwd.value) {
+            if (confirm("注册成功")) {
+                window.open("index.html", "_self");
+            }
+        }else {
+            alert("密码不一致");
+        }
+    }
+}
